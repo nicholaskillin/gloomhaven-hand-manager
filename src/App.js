ec3acff02b2f742207bb8478d01d895e8c38c13b
Nick Killin <nicholaskillin@gmail.com>
2020-06-06 09:50:04 -0400

    get rid of unused useEffect import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import _ from 'lodash'
 import CharacterSelection from './CharacterSelection'
