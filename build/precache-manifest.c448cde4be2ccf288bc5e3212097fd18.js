48622c75bab37c0d4f4a1287c004082c0d7eb4aa
Nick Killin <nicholaskillin@gmail.com>
2020-06-08 06:45:07 -0400

    finish merging react app

---
+++ build/precache-manifest.c448cde4be2ccf288bc5e3212097fd18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2742c7c0c00bf4a4e1af9f7d0f70dba3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6a71939283d56e4411d",
+    "url": "/static/css/main.221ad936.chunk.css"
+  },
+  {
+    "revision": "1425417d9c975d2d2abd",
+    "url": "/static/js/2.430f5ca1.chunk.js"
+  },
+  {
+    "revision": "f86dc4ed6c239e9e855e2aab8e580a21",
+    "url": "/static/js/2.430f5ca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6a71939283d56e4411d",
+    "url": "/static/js/main.dc543221.chunk.js"
+  },
+  {
+    "revision": "fd36c86ae14a24275487",
+    "url": "/static/js/runtime-main.c48cb2eb.js"
+  },
+  {
+    "revision": "c22a259af9f36e58fef8197d9a8d3a08",
+    "url": "/static/media/background.c22a259a.png"
+  }
+]);
