647f9a0a5fc549a1118cfdf74a435002c830bcef
Nick Killin <39142975+nicholaskillin@users.noreply.github.com>
2021-10-14 07:18:28 -0400

    Refactor feedback modal (#155)
    * Add and update packages
    * Use jest-canvas-mock and mutation observer shim in tests
    * Extract feedback modal and refactor using UI-kit
    * prettier
    * Fix import on spec
    * Use yarn not npm

---
+++ src/__tests__/FeedbackModal.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import FeedbackModal from '../FeedbackModal'
+import { render, screen } from '@testing-library/react'
+
+it('has a Subject field and label', () => {
+  render(<FeedbackModal show={true} />)
+  expect(screen.getByText('Subject')).toBeInTheDocument()
+  expect(screen.getByTestId('subjectInput')).toBeInTheDocument()
+})
+
+it('has a Type field', () => {
+  render(<FeedbackModal show={true} />)
+  expect(screen.getByText('Type')).toBeInTheDocument()
+  expect(screen.getByTestId('typeInput')).toBeInTheDocument()
+})
+
+it('has a Description field', () => {
+  render(<FeedbackModal show={true} />)
+  expect(screen.getByText('Description')).toBeInTheDocument()
+  expect(screen.getByTestId('descriptionInput')).toBeInTheDocument()
+})
